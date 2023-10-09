e048605cc65bbd7a6339b2725a26db14d9704d7e
Samuel Palacio <82546723+samuelpalacio1709@users.noreply.github.com>
2023-10-08 21:57:32 -0500

    fix inputs

--- src/character.js
+++ src/character.js
@@ -19,10 +19,9 @@ export class Character {
         this.mesh.castShadow = true;
         this.mesh.position.y = 2;
         this.scene.add(this.mesh);
-        this.maxSpeed = 3;
+        this.maxSpeed = 5;
         this.force = new CANNON.Vec3(0, 0, 0)
         //Physics
-        const radius = 1; // m
         this.body = new CANNON.Body({
             mass: 1, // kg
             position: new CANNON.Vec3(0, 2, 0), // m
@@ -34,7 +33,8 @@ export class Character {
         this.body.updateMassProperties();
         this.body.material.friction = 0;
         this.world.addBody(this.body);
-        document.addEventListener('keypress', this.handleInputDown.bind(this));
+        this.keySet = new Set();
+        document.addEventListener('keydown', this.handleInputDown.bind(this));
         document.addEventListener('keyup', this.handleInputUp.bind(this));
 
 
@@ -42,52 +42,62 @@ export class Character {
 
 
     update() {
+        this.setForce();
         this.body.velocity = new CANNON.Vec3(this.force.x, this.body.velocity.y, this.force.z)
-
         this.mesh.position.copy(this.body.position);
         this.mesh.quaternion.copy(this.body.quaternion);
 
     }
 
     handleInputDown(event) {
-        if (this.force.z === 0) {
-            if (event.key.toLowerCase() === 'w') {
-
-                this.force.z = 1 * this.maxSpeed;
-            }
-            else if (event.key.toLowerCase() === 's') {
-                this.force.z = -1 * this.maxSpeed;;
-            }
-        }
-
-        if (this.force.x === 0) {
-            if (event.key.toLowerCase() === 'd') {
-                this.force.x = -1 * this.maxSpeed;;
-
-            } else if (event.key.toLowerCase() === 'a') {
-                this.force.x = 1 * this.maxSpeed;;
-            }
-        }
-
-
+        this.keySet.add(event.key.toLowerCase())
     }
 
     handleInputUp(event) {
+        this.keySet.delete(event.key.toLowerCase())
+    }
 
+    setForce() {
+        this.force.z = 0;
+        this.force.x = 0;
 
-        if (event.key.toLowerCase() === 'w' && this.force.z > 0) {
+        if (this.keySet.has('w')) {
+            this.force.z = 1;
+        }
+
+        if (this.keySet.has('s')) {
+            this.force.z = -1;
+        }
+
+        if (this.keySet.has('a')) {
+            this.force.x = 1;
+
+        }
+
+        if (this.keySet.has('d')) {
+            this.force.x = -1;
+        }
+
+        if (this.keySet.has('d') && this.keySet.has('a')) {
+            this.force.x = 0;
             this.force.z = 0;
         }
-        else if (event.key.toLowerCase() === 's' && this.force.z < 0) {
+        if (this.keySet.has('w') && this.keySet.has('s')) {
+            this.force.x = 0;
             this.force.z = 0;
         }
 
-        if (event.key.toLowerCase() === 'd' && this.force.x < 0) {
-            this.force.x = 0;
+        // You can normalize the vector if needed
+        const length = Math.sqrt(this.force.z * this.force.z + this.force.x * this.force.x);
 
-        } else if (event.key.toLowerCase() === 'a' && this.force.x > 0) {
-            this.force.x = 0;
+        if (length !== 0) {
+            this.force.x /= length;
+            this.force.z /= length;
+            this.force.x *= this.maxSpeed;
+            this.force.z *= this.maxSpeed;
         }
+
+
     }
 }
 
